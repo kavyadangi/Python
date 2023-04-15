5937e59344e4669cef8cbe25dffa2fdccdb9c803
Kavya <kavyadangi09@gmail.com>
2023-04-15 10:30:21 +0530

    Create inheritencecodeno2.py

---
+++ Classes and Objects/inheritencecodeno2.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self,name,program,batch,rollno):
+        self.name=name
+        self.program=program
+        self.batch=int(batch)
+        self.rollno=int(rollno)
+        print(self.name,self.program,self.batch,self.rollno)
+    def Studentw(self,filename):
+        self.List=[self.name,self.program,self.batch,self.rollno)
+        inheritence.txt=(self.name,"",self.program,self.batch,self.rollno
+        
