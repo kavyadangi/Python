0037a7a7825bfa0ba19068cf70d08fc951daa367
Kavya <kavyadangi09@gmail.com>
2023-04-16 16:08:07 +0530

    Create init code.py

---
+++ Classes and Objects/init code.py
@@ -0,0 +1,11 @@
+class :
+  def __init__(self, name, sap_id):
+    self.name = name
+    self.sap_id = sap_id
+
+p1 = Person("Kavya",500108583)
+
+print(p1.name)
+print(p1.sap_id)
+#def displayPerson(self):
+    #print("Name:"+self.name+"sap id:"+self.sap_id)
