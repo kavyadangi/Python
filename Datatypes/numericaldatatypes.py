77dc6d8748130aa59839a8e33ba55d9badae3d8d
Kavya <kavyadangi09@gmail.com>
2023-04-16 16:08:15 +0530

    Create numericaldatatypes.py

---
+++ Datatypes/numericaldatatypes.py
@@ -0,0 +1,17 @@
+#numeric data types : int, float, complex
+#string data types : str
+#sequence data types : list, tuple, range
+#binary data types : bytes, bytearray, memoryview
+#mapping data types : dict
+#boolean data types : bool
+#set data types : set, frozenset
+
+#creating a variable with integer value
+a=100
+print("The type of variable having value", a, "is", type(a))
+#creating a variable with float value
+b=10.2345
+print("The type of variable having value", b, "is", type(b))
+#creating a variable with complex value
+c=100+3j
+print("The type of variable having value", c, "is", type(c))
