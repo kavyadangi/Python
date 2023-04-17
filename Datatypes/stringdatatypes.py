ffc818250484fb202e065d099eed8c6bebff8a42
Kavya <kavyadangi09@gmail.com>
2023-04-17 20:41:46 +0530

    Create stringdatatypes.py

---
+++ Datatypes/stringdatatypes.py
@@ -0,0 +1,10 @@
+a="String in a double quote"
+b='String in a single quote'
+print(a)
+print(b)
+
+#using ',' to concatenate the two or several strings
+print(a, "concatenated with", b)
+
+#using '+' to concatenate the two or several strings
+print(a+ " concated with " +b)
