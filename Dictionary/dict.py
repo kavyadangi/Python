2ca412e4f88bdf489a6aa77a4c662ff0b82f686e
Kavya <kavyadangi09@gmail.com>
2023-05-02 16:08:04 +0530

    Create dict.py

---
+++ Dictionary/dict.py
@@ -0,0 +1,13 @@
+#Dictionaries are used to store data values in key:value pairs.
+
+#A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+
+#As of Python version 3.7, dictionaries are ordered. In Python 3.6 and earlier, dictionaries are unordered.
+
+#Dictionaries are written with curly brackets, and have keys and values:
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
