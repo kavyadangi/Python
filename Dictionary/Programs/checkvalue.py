c298633e4d5afb85b700421111a19c891768685c
Kavya <kavyadangi09@gmail.com>
2023-05-08 23:33:28 +0530

    Create checkvalue.py

---
+++ Dictionary/Programs/checkvalue.py
@@ -0,0 +1,5 @@
+#Write a Python program to check if value 200 exists in the following dictionary.
+#sample_dict = {'a': 100, 'b': 200, 'c': 300}
+sample_dict = {'a': 100, 'b': 200, 'c': 300}
+if 200 in sample_dict.values():
+    print("200 Present")
