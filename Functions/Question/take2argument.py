952ed333351a020503ab3887dc7d9490d5f84f75
Kavya <kavyadangi09@gmail.com>
2023-05-12 03:06:39 +0530

    Create take2argument.py

---
+++ Functions/Question/take2argument.py
@@ -0,0 +1,5 @@
+#WAP to create a function that takes in two arguments, name and age, and print their value.
+def myfunc(name, age):
+    print("Name :", name)
+    print("Age :", age)
+myfunc("Samaksh", 18)
