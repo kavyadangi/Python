d031ba0fdd94ce80cc7fcb6cd57a64258834f732
Kavya <kavyadangi09@gmail.com>
2023-05-18 13:30:19 +0530

    Create q7.py

---
+++ Lab Manual/Practical 1/q7.py
@@ -0,0 +1,12 @@
+name="SAMAKSH MITTAL"
+sap=500107995
+dob="31 July 2004"
+address="UPES\n\tBidholi Campus\n\tPincode : 248007"
+programme="AI & ML"                                                                                     
+semester=2
+print("NAME :",name)
+print("SAP ID :", sap)
+print("DATE OF BIRTH :", dob)
+print("ADDRESS :", address)
+print("PRAGRAMME :", programme)
+print("SEMESTER :", semester)
