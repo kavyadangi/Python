4e8ff28471bd5254916c2cf1a4f9036bd2bb338e
Kavya <kavyadangi09@gmail.com>
2023-05-22 19:40:58 +0530

    Create forcomprehensionquestion2.py

---
+++ List/forcomprehensionquestion2.py
@@ -0,0 +1,4 @@
+#set all values in the new list to hello
+fruits=["apple", "banana", "cherry", "kiwi", "mango"]
+newlist=['hello' for x in fruits]
+print(newlist)                                  #list comprehension
