038a15a05325e5de200bc32ac1ef021cb4166650
Kavya <kavyadangi09@gmail.com>
2023-05-22 19:40:43 +0530

    Create for1.py

---
+++ List/for1.py
@@ -0,0 +1,3 @@
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+    print(thislist[i])       #the ranege function returns a sequence of numbers starting from 0 by default and increments by 1 by default and ends at a specified number
