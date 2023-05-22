762f856412bbc9dbc9623f058e476074696c2ef6
Kavya <kavyadangi09@gmail.com>
2023-05-22 19:40:54 +0530

    Create forcomprehensionquestion1.py

---
+++ List/forcomprehensionquestion1.py
@@ -0,0 +1,4 @@
+#set the values in new list to upper case
+fruits=["apple", "banana", "cherry", "kiwi", "mango"]
+newlist=[x.upper() for x in fruits]
+print(newlist)                                  #list comprehension
