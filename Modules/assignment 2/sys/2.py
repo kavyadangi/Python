92723240d31cc031f6eea77a239da2efeaf2aafb
Kavya <kavyadangi09@gmail.com>
2023-05-25 00:29:15 +0530

    Create 2.py

---
+++ Modules/assignment 2/sys/2.py
@@ -0,0 +1,6 @@
+import sys
+print("Python version: ", sys.version)
+print("Python version info: ", sys.version_info)
+print("Python executable path: ", sys.executable)
+print("Platform: ", sys.platform)
+print("Loaded modules: ", sys.modules.keys())
