a005c5d0a1a84e6dc0b5b710bef81e9c5c724da8
Kavya <kavyadangi09@gmail.com>
2023-05-24 17:22:48 +0530

    Create sort3.py

---
+++ List/sort3.py
@@ -0,0 +1,3 @@
+thislist=["banana", "Orange", "Kiwi", "cherry"]
+thislist.sort(key=str.lower)    #if you want a case insensitive sort function we can use str.lower as a key function
+print(thislist)
