bf2fba419f5507c5481bf9f3bc86b61cdf867c6e
Kavya <kavyadangi09@gmail.com>
2023-06-07 22:38:49 +0530

    Create absolutevalue.py

---
+++ Programs/absolutevalue.py
@@ -0,0 +1,6 @@
+#Wap to print absolute value of a number entered by user
+number=int(input("Enter number"))
+if(number<0):
+    print("Absolute value of number is", abs(number))
+else:
+    print("Absolute value of number is", number)
