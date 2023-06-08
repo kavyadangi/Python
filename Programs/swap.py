6970d685aa552f757e2da852c33c511b8899ac7b
Kavya <kavyadangi09@gmail.com>
2023-06-09 00:08:42 +0530

    Create swap.py

---
+++ Programs/swap.py
@@ -0,0 +1,7 @@
+a=input("Enter 1st variable")
+b=input("Enter 2nd variable")
+print("Two numbers before swapping are : a =", a, "and b =", b)
+t=b
+b=a
+a=t
+print("Two numbers after swapping are : a =", a, "and b =", b)
