e75fbe74ba00c14e6664d729a10ea26dbe206960
Kavya <kavyadangi09@gmail.com>
2023-06-14 16:18:15 +0530

    Create set.py

---
+++ Set/set.py
@@ -0,0 +1,6 @@
+#set items are unordered, unchangeable, and do not allow duplicate values.
+#Set items are unordered, unchangeable, and do not allow duplicate values.
+#set elements cant be referred with index number
+thisset={"apple","banana","cherry"}
+print(thisset)
+
