dd572351d515b570d8fb14f826da9f1a597f36d7
Kavya <kavyadangi09@gmail.com>
2023-08-21 23:29:36 +0530

    Create using_func.py

---
+++ MAtplotlib/using_func.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+name=['kavya','katrina','karishma']
+marks=[90,85,95]
+plt.xlabel("Name")
+plt.ylabel("Marks")
+plt.plot(name,marks)
+plt.title("student marks")
+plt.grid(False)
+plt.xticks("kuchu")
+plt.show()
+
